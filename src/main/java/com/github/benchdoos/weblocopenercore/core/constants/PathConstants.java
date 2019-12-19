b5dae125a71806e1ceefd8883dc7cee248f91d3a
Eugene Zrazhevsky <evgeny.zrazhevskiy@rtlabs.ru>
2019-12-19 08:41:16 +0300

    Updated recent files path

--- src/main/java/com/github/benchdoos/weblocopenercore/core/constants/PathConstants.java
+++ src/main/java/com/github/benchdoos/weblocopenercore/core/constants/PathConstants.java
@@ -28,5 +28,6 @@ public interface PathConstants {
     String UPDATE_PATH_FILE = System.getProperty("java.io.tmpdir") + File.separator + WEBLOCOPENER_APPLICATION_NAME + File.separator;
 
     String RECENT_OPENED_FILES_FILE_PATH = System
-            .getProperty("java.io.tmpdir") + File.separator + WEBLOCOPENER_APPLICATION_NAME + File.separator + "recent-files.json";
+            .getProperty("java.io.tmpdir") + File.separator + WEBLOCOPENER_APPLICATION_NAME + File.separator
+            + System.getProperty("user.name") + "-recent-files.json";
 }
