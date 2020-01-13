1e8b93e2b57a5c415eebc9d584eb297432fd5c87
Eugene Zrazhevsky <evgeny.zrazhevskiy@rtlabs.ru>
2020-01-13 16:53:04 +0300

    Code refactoring

--- src/main/java/com/github/benchdoos/weblocopenercore/utils/CoreUtils.java
+++ src/main/java/com/github/benchdoos/weblocopenercore/utils/CoreUtils.java
@@ -17,6 +17,7 @@ package com.github.benchdoos.weblocopenercore.utils;
 
 import com.github.benchdoos.weblocopenercore.Main;
 import com.github.benchdoos.weblocopenercore.core.constants.ApplicationConstants;
+import com.github.benchdoos.weblocopenercore.gui.InfoDialog;
 import com.github.benchdoos.weblocopenercore.utils.version.ApplicationVersion;
 import com.github.benchdoos.weblocopenercore.utils.version.Beta;
 import lombok.extern.log4j.Log4j2;
@@ -29,9 +30,13 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.URL;
 import java.util.Properties;
 import java.util.Scanner;
+
+import static com.github.benchdoos.weblocopenercore.core.constants.ApplicationConstants.WEBLOCOPENER_APPLICATION_NAME;
 
 @Log4j2
 public class CoreUtils {
@@ -226,5 +231,38 @@ public class CoreUtils {
             log.warn("Could not load application ApplicationVersion info", e);
             return applicationVersion;
         }
+    }
+
+    public static void showFatalErrorMessage(Exception e) {
+        InfoDialog infoDialog = new InfoDialog();
+        infoDialog.setTitle(WEBLOCOPENER_APPLICATION_NAME);
+        //todo load this from file
+        // todo add reporting errors??? how - hz...
+        final String content = "<html>\n" +
+                "<body>\n" +
+                "<h1>\n" +
+                "    Application failed with exception\n" +
+                "</h1>\n" +
+                "<br>\n" +
+                "Create your issue <a href=\"https://github.com/benchdoos/WeblocOpener/issues/new/choose\">here</a>.\n" +
+                "<br>\n" +
+                "Don't forget to copy stacktrace:\n" +
+                "<br>\n" +
+                "<br>\n" +
+                "<br>\n" +
+                "<table>\n" +
+                "    <tr>\n" +
+                "        <td>%s</td>\n" +
+                "    </tr>\n" +
+                "</table>\n" +
+                "</body>\n" +
+                "</html>";
+
+        final StringWriter stringWriter = new StringWriter();
+        final PrintWriter printWriter = new PrintWriter(stringWriter);
+        e.printStackTrace(printWriter);
+
+        infoDialog.setContent(String.format(content, stringWriter.toString()));
+        infoDialog.setVisible(true);
     }
 }
