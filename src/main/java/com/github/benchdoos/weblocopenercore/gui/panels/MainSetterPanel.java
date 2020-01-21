51993847d06a2c2cf24ae2b0ca4fe2a997394816
Eugene Zrazhevsky <evgeny.zrazhevskiy@rtlabs.ru>
2020-01-21 18:39:30 +0300

    Added share user info setting

--- src/main/java/com/github/benchdoos/weblocopenercore/gui/panels/MainSetterPanel.java
+++ src/main/java/com/github/benchdoos/weblocopenercore/gui/panels/MainSetterPanel.java
@@ -66,6 +66,7 @@ public class MainSetterPanel extends JPanel implements SettingsPanel, Translatab
     private JButton aboutButton;
     private JCheckBox savePreviouslyOpenedFilesCheckBox;
     private JButton cleanupRecentFilesButton;
+    private JCheckBox shareInfoCheckBox;
     private String launcherLocationPath;
 
     public MainSetterPanel() {
@@ -130,7 +131,7 @@ public class MainSetterPanel extends JPanel implements SettingsPanel, Translatab
         panel1.setLayout(new GridLayoutManager(4, 1, new Insets(0, 0, 0, 0), -1, -1));
         contentPane.add(panel1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         final JPanel panel2 = new JPanel();
-        panel2.setLayout(new GridLayoutManager(5, 3, new Insets(0, 0, 0, 0), -1, -1));
+        panel2.setLayout(new GridLayoutManager(6, 3, new Insets(0, 0, 0, 0), -1, -1));
         panel1.add(panel2, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         openFolderForQRCheckBox = new JCheckBox();
         openFolderForQRCheckBox.setSelected(true);
@@ -142,7 +143,7 @@ public class MainSetterPanel extends JPanel implements SettingsPanel, Translatab
         panel2.add(showNotificationsToUserCheckBox, new GridConstraints(2, 0, 1, 3, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final JPanel panel3 = new JPanel();
         panel3.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
-        panel2.add(panel3, new GridConstraints(4, 0, 1, 3, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        panel2.add(panel3, new GridConstraints(5, 0, 1, 3, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         final Spacer spacer1 = new Spacer();
         panel3.add(spacer1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         savePreviouslyOpenedFilesCheckBox = new JCheckBox();
@@ -157,6 +158,10 @@ public class MainSetterPanel extends JPanel implements SettingsPanel, Translatab
         panel2.add(spacer2, new GridConstraints(3, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         final JSeparator separator1 = new JSeparator();
         panel2.add(separator1, new GridConstraints(0, 0, 1, 3, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        shareInfoCheckBox = new JCheckBox();
+        this.$$$loadButtonText$$$(shareInfoCheckBox, ResourceBundle.getBundle("translations/ShareAnonymousInfoBundle").getString("shareUserInfoLabel"));
+        shareInfoCheckBox.setToolTipText(ResourceBundle.getBundle("translations/ShareAnonymousInfoBundle").getString("message"));
+        panel2.add(shareInfoCheckBox, new GridConstraints(4, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final Spacer spacer3 = new Spacer();
         panel1.add(spacer3, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         final JPanel panel4 = new JPanel();
@@ -385,6 +390,9 @@ public class MainSetterPanel extends JPanel implements SettingsPanel, Translatab
 
         cleanupRecentFilesButton.setText(translation.getTranslatedString("cleanRecentFilesButtonText"));
         cleanupRecentFilesButton.setToolTipText(translation.getTranslatedString("cleanRecentFilesButtonToolTip"));
+
+        shareInfoCheckBox.setText(Translation.getTranslatedString("ShareAnonymousInfoBundle", "shareUserInfoLabel"));
+        shareInfoCheckBox.setToolTipText(Translation.getTranslatedString("ShareAnonymousInfoBundle", "message"));
 
         translateLanguagePanel();
     }
