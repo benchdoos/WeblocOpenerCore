e5c48f1b4a5efe3fed4dcf4291db5e96387fdad5
Eugene Zrazhevsky <eugeny.zrazhevsky@gmail.com>
2020-07-13 15:07:07 +0300

    WeblocOpenerCore-2: Updated buttons color

--- src/main/java/com/github/benchdoos/weblocopenercore/gui/AboutApplicationDialog.java
+++ src/main/java/com/github/benchdoos/weblocopenercore/gui/AboutApplicationDialog.java
@@ -16,7 +16,6 @@
 package com.github.benchdoos.weblocopenercore.gui;
 
 import com.github.benchdoos.weblocopenercore.core.Translation;
-import com.github.benchdoos.weblocopenercore.core.constants.ApplicationConstants;
 import com.github.benchdoos.weblocopenercore.core.constants.PathConstants;
 import com.github.benchdoos.weblocopenercore.core.constants.StringConstants;
 import com.github.benchdoos.weblocopenercore.gui.buttons.DonationButton;
@@ -38,7 +37,6 @@ import net.java.balloontip.styles.MinimalBalloonStyle;
 import net.java.balloontip.utils.TimingUtils;
 
 import javax.swing.AbstractButton;
-import javax.swing.Action;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -58,11 +56,7 @@ import java.awt.Insets;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
@@ -486,10 +480,11 @@ public class AboutApplicationDialog extends JDialog {
     }
 
     private String createHtmlLink(String string) {
+        final String content = "<html><a style=\"color:{};\" href=\"\">" + string + "</a></html>";
         if (!PreferencesManager.isDarkModeEnabledNow()) {
-            return "<html><a href=\"\">" + string + "</a></html>";
+            return content.replace("{}", "#00598f");
         } else {
-            return "<html><a style=\"color:#BAE5FF;\" href=\"\">" + string + "</a></html>";
+            return content.replace("{}", "#BAE5FF");
         }
     }
 
